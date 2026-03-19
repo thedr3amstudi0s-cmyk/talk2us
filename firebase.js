e89fdbc5c24e874d463b097117f210296914f874
thedr3amstudi0s-cmyk <thedr3am.studi0s@gmail.com>
2026-03-18 21:44:46 -0400

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,40 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getDatabase, ref, set, push, onChildAdded, update, onValue } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDig_wBEJqbJf44X15zxbI9-5e-TRNcTKg",
+  authDomain: "project-c7a8c42f-cf98-4b6e-82c.firebaseapp.com",
+  databaseURL: "https://project-c7a8c42f-cf98-4b6e-82c-default-rtdb.firebaseio.com",
+  projectId: "project-c7a8c42f-cf98-4b6e-82c",
+  storageBucket: "project-c7a8c42f-cf98-4b6e-82c.firebasestorage.app",
+  messagingSenderId: "928822058212",
+  appId: "1:928822058212:web:81fe04b3464206529c60f1",
+  measurementId: "G-Z13ZTB32TX"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+// Guest creates request
+function createGuestRequest(guestId) {
+  set(ref(db, 'requests/' + guestId), { guestId, active:true });
+}
+
+// Send message
+function sendMessage(chatId, sender, text) {
+  push(ref(db, 'chats/' + chatId), { sender, text, time: Date.now() });
+}
+
+// Listen messages
+function listenMessages(chatId, callback) {
+  onChildAdded(ref(db, 'chats/' + chatId), snapshot => {
+    callback(snapshot.val());
+  });
+}
+
+// Mod marks guest active
+function markGuestActive(guestId) {
+  update(ref(db, 'requests/' + guestId), { active:true });
+}
+
+export { db, createGuestRequest, sendMessage, listenMessages, markGuestActive, ref, onValue };
